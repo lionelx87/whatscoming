8d5e8493d95f8d84b80768f3b3a38970422a7fce
Lionel Acosta <lionelx87@gmail.com>
2024-06-10 00:03:37 -0300

    chore: add prettier config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-center'>Hola Prueba</h1>
+      <h1 className="text-center">Hola Prueba</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
