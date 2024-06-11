17445aad12cd7d512a4149382741e52f45031b7f
Lionel Acosta <lionelx87@gmail.com>
2024-06-10 22:42:30 -0300

    feat: changed logo

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,11 @@
-import Logo from '../../../src/assets/images/logo.svg'
+import Logo from '../../../src/assets/images/logo.png'
 import { SunIcon } from '../icons'
 
 export const Header = () => {
     return (
         <div className="flex justify-between container mx-auto px-4 py-4">
             <h1>
-                <img src={Logo} alt="" />
+                <img src={Logo} alt="Logo" className="w-40" />
             </h1>
             <button>
                 <SunIcon />
