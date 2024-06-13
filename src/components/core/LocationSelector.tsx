a30288514c5142cf113394a1eee9035b588b8632
Lionel Acosta <lionelx87@gmail.com>
2024-06-12 22:24:40 -0300

    feat: create LocationSelector component

---
+++ src/components/core/LocationSelector.tsx
@@ -0,0 +1,24 @@
+import { SyntheticEvent, useState } from "react";
+
+export const LocationSelector = () => {
+
+  const [location, setLocation] = useState('')
+
+  const searchLocation = (e: SyntheticEvent) => {
+    e.preventDefault()
+    const normalizeLocation = location.trim()
+    if(!normalizeLocation) return
+    setLocation('')
+  }
+  return (
+    <form onSubmit={searchLocation} className="mx-auto w-full max-w-xl">
+      <input
+        type="text"
+        placeholder="Ingrese su ubicación"
+        className="mx-auto w-full max-w-xl rounded-lg border-0 bg-[#1E1E29] p-5 text-[#7F7F98] outline-none"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+    </form>
+  );
+};
--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -1,13 +1,9 @@
+import { LocationSelector } from "../core/LocationSelector"
+
 export const Main = () => {
     return (
         <div className="container mx-auto px-4 py-4 flex-1 flex justify-center items-center">
-            <form className="w-full max-w-xl mx-auto">
-                <input 
-                    type="text" 
-                    placeholder="Ingrese su ubicación" 
-                    className="rounded-lg p-5 outline-none border-0 bg-[#1E1E29] text-[#7F7F98] w-full max-w-xl mx-auto"
-                />
-            </form>
+            <LocationSelector />
         </div>
     )
 }
