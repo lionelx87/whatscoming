f46c8a55f6f94d61376b94538c9955fbf5a4eeb2
Lionel Acosta <lionelx87@gmail.com>
2024-06-14 00:43:02 -0300

    feat: remove custom color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,6 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#FFFFFF',
-      'gray-600': '#1E1E29',
-      'gray-400': '#7F7F98',
-    },
     extend: {},
   },
   plugins: [],
