d47408c38aed8abc22604af519ffc21debdbd395
Lionel Acosta <lionelx87@gmail.com>
2024-06-14 01:06:42 -0300

    feat: apply custom colors

--- src/components/core/LocationSelector.tsx
+++ src/components/core/LocationSelector.tsx
@@ -15,7 +15,7 @@ export const LocationSelector = () => {
       <input
         type="text"
         placeholder="Ingrese su ubicación"
-        className="mx-auto w-full max-w-xl rounded-lg border-0 bg-gray-600 p-5 text-gray-400 outline-none"
+        className="mx-auto w-full max-w-xl rounded-lg border-0 bg-[#1E1E29] p-5 text-[#7F7F98] outline-none"
         value={location}
         onChange={(e) => setLocation(e.target.value)}
       />
