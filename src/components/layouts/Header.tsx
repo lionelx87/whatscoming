2b02a62e27058eda60677d8c323b059068d26547
Lionel Acosta <lionelx87@gmail.com>
2024-06-15 00:01:59 -0300

    feat: toggles between sun and moon icon in dark mode

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
 import Logo from '../../../src/assets/images/logo.png'
-import { SunIcon } from '../icons'
+import { MoonIcon, SunIcon } from '../icons'
 
 export const Header = () => {
+
+    const [darkMode, setDarkMode] = useState(false)
+
+    // const handleClickToggleTheme = () => {}
+
     return (
         <div className="flex justify-between container mx-auto px-4 py-4">
             <h1>
                 <img src={Logo} alt="Logo" className="w-40" />
             </h1>
-            <button>
-                <SunIcon />
+            <button onClick={() => setDarkMode(!darkMode)}>
+                {
+                    darkMode ? <SunIcon /> : <MoonIcon />
+                }                
             </button>
         </div>
     )
--- src/features/dark-mode.ts
+++ src/features/dark-mode.ts
@@ -5,8 +5,10 @@ if (
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
   document.documentElement.classList.add("dark");
+  localStorage.theme = "dark";
 } else {
   document.documentElement.classList.remove("dark");
+  localStorage.theme = "light";
 }
 
 // Whenever the user explicitly chooses light mode
