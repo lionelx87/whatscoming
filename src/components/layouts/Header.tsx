ccf64bdf551cc7bdd6604bc4ef428fced04559b1
Lionel Acosta <lionelx87@gmail.com>
2024-06-15 10:56:16 -0300

    feat: dark mode with initial state

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,19 +2,21 @@ import { useEffect, useState } from 'react'
 import Logo from '../../../src/assets/images/logo.png'
 import { MoonIcon, SunIcon } from '../icons'
 
+const initialStateDarkMode = localStorage.getItem("theme") === "dark"
+
 export const Header = () => {
 
-    const [darkMode, setDarkMode] = useState(true)
+    const [darkMode, setDarkMode] = useState(initialStateDarkMode)
 
     useEffect(() => {
         if(darkMode) {
             document.documentElement.classList.add("dark")
+            localStorage.setItem("theme", "dark")
         } else {
             document.documentElement.classList.remove("dark")
+            localStorage.setItem("theme", "light")
         }
     }, [darkMode])
-
-    // const handleClickToggleTheme = () => {}
 
     return (
         <div className="flex justify-between container mx-auto px-4 py-4">
