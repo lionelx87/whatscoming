ddfdfd5c70a573d1e9a29a8642444df9fe29aa02
Lionel Acosta <lionelx87@gmail.com>
2024-06-21 21:36:05 -0300

    feat: add icons for detail

--- src/components/modules/weather/WeatherDisplay.tsx
+++ src/components/modules/weather/WeatherDisplay.tsx
@@ -6,6 +6,7 @@ type WeatherDisplayProps = {
 };
 export const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ data }) => {
   return (
+    <>
     <div className="flex h-1/3 flex-col rounded-lg bg-[url('./assets/images/weather-bg-mobile.svg')] bg-cover bg-center bg-no-repeat">
       <h1 className="pt-2 text-center text-lg">{data.location.name}</h1>
       <p className="text-md text-center">{data.location.localtime}</p>
@@ -19,5 +20,15 @@ export const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ data }) => {
         </div>
       </div>
     </div>
+    <div className="mt-4 dark:bg-custom-600 dark:text-custom-400 rounded-lg">
+      <div>
+        <div>
+          <img src="" alt="" />
+          <span>Prueba</span>
+        </div>
+        <div>26</div>
+      </div>  
+    </div>    
+    </>
   );
 };
