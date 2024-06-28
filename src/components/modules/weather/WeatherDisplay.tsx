f75ab25edcf18cc29f116d3ea8f59a8d34e06d5f
Lionel Acosta <lionelx87@gmail.com>
2024-06-28 19:35:43 -0300

    feat: add mock structure for 5-day forecasting

--- src/components/modules/weather/WeatherDisplay.tsx
+++ src/components/modules/weather/WeatherDisplay.tsx
@@ -71,6 +71,18 @@ export const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ data }) => {
           </div>
         ))}
       </div>
+      <div className="mt-4 flex justify-between gap-3 rounded-lg p-2 dark:bg-custom-600 dark:text-custom-400">
+        {
+          [0, 1, 2, 3, 4].map((elem) => (
+          <div key={elem} className="flex flex-col items-center gap-2">
+              <span>28/05</span>
+              <img src={data.current.condition.icon} alt="Icono" />
+              <span className="text-red-400">MAX</span>
+              <span className="text-blue-500">MIN</span>
+            </div>
+          ))
+        }
+      </div>
     </>
   );
 };
