e0b6ad5fb6eeae3e6135976dd79759f51c57608c
Lionel Acosta <lionelx87@gmail.com>
2024-07-06 11:21:00 -0300

    fix: overlap with footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,5 +1,15 @@
 export const Footer = () => {
   return (
-    <p className="p-4 text-center text-white">Developed by Lionel Acosta</p>
+    <p className="p-4 text-center text-white">
+      Developed by{" "}
+      <a
+        className="text-blue-500"
+        href="https://github.com/lionelx87"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Lionel Acosta
+      </a>
+    </p>
   );
 };
